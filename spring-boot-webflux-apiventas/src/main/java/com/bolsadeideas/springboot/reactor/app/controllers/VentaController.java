2ffbf9e2fa5be8698a544cee5ce19d9aa40d874f
Andres Jurado <jajurado@bancolombia.com.co>
2025-04-29 17:50:58 -0500

    ajuste

--- spring-boot-webflux-apiventas/src/main/java/com/bolsadeideas/springboot/reactor/app/controllers/VentaController.java
+++ spring-boot-webflux-apiventas/src/main/java/com/bolsadeideas/springboot/reactor/app/controllers/VentaController.java
@@ -63,54 +63,6 @@ public class VentaController {
         return ventaService.buscarPorId(id);
     }
 
-/*	@PostMapping
-	public Mono<ResponseEntity<Map<String, Object>>> vender(@Valid @RequestBody Mono<Venta> monoVenta) {
-		Map<String, Object> respuesta = new HashMap<>();
-		return monoVenta.flatMap(venta -> {
-			if (venta.getFechaVenta() == null) {
-				LocalDate date = LocalDate.now();
-				LocalTime time = LocalTime.now();
-				LocalDateTime localDateTime = LocalDateTime.of(date, time);
-				venta.setFechaVenta(localDateTime);
-			}
-			return Mono.just(venta);
-		}).flatMap(vt -> {
-			String p = vt.getProductosCodStock().get(0);
-			String codigo = p.split(":")[0];
-			String stock = p.split(":")[1];
-			return productoService.buscarPorCodigo(codigo).map(prd -> {
-				if (Integer.parseInt(stock) > prd.getStock()) {
-					System.out.println("***********Error Stock mayor! prd.getStock() : " + prd.getStock());
-					//respuesta.put("mensaje", "Error Stock mayor! :" + vt.getId());
-					//respuesta.put("timestamp", new Date());
-					//respuesta.put("status", HttpStatus.BAD_REQUEST.value());
-					return vt;
-					// return Mono.error(new IllegalStateException("Error Stock mayor!
-					// prd.getStock()"));
-				}
-				return null;
-			});
-		}).flatMap(t -> {
-			return ventaService.guardar(t).flatMap(v -> {
-				respuesta.put("venta", v);
-				respuesta.put("mensaje", "Venta exitosa! :" + v.getId());
-				respuesta.put("timestamp", new Date());
-				return Mono.just(ResponseEntity.created(URI.create("/api/ventas".concat(v.getId())))
-						.contentType(MediaType.APPLICATION_JSON_UTF8).body(respuesta));
-			});
-		}).onErrorResume(t -> {
-			return Mono.just(t).cast(WebExchangeBindException.class).flatMap(e -> Mono.just(e.getFieldErrors()))
-					.flatMapMany(Flux::fromIterable)
-					.map(fieldError -> "El campo " + fieldError.getField() + " " + fieldError.getDefaultMessage())
-					.collectList().flatMap(list -> {
-						respuesta.put("errors", list);
-						respuesta.put("timestamp", new Date());
-						respuesta.put("status", HttpStatus.BAD_REQUEST.value());
-						return Mono.just(ResponseEntity.badRequest().body(respuesta));
-					});
-		});
-	}*/
-
     @PostMapping
     public Mono<ResponseEntity<Map<String, Object>>> vender(@Valid @RequestBody Mono<Venta> monoVenta) {
         Map<String, Object> respuesta = new HashMap<>();
